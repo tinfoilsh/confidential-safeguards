f8901003c827325f118593491ebf88f33795f53c
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-09-11 15:10:18 -0700

    chore: log with the standard library

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.27.1
 
 require (
 	github.com/openai/openai-go/v3 v3.55.0
-	github.com/sirupsen/logrus v1.10.2
 	github.com/tinfoilsh/tinfoil-go v0.15.3
 )
 
@@ -60,6 +59,7 @@ require (
 	github.com/sigstore/sigstore v1.10.8 // indirect
 	github.com/sigstore/sigstore-go v1.2.2 // indirect
 	github.com/sigstore/timestamp-authority/v2 v2.1.3 // indirect
+	github.com/sirupsen/logrus v1.10.2 // indirect
 	github.com/theupdateframework/go-tuf/v2 v2.4.2 // indirect
 	github.com/tidwall/gjson v1.19.0 // indirect
 	github.com/tidwall/match v1.2.0 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"errors"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -10,7 +12,6 @@ import (
 	"time"
 
 	"github.com/openai/openai-go/v3/option"
-	log "github.com/sirupsen/logrus"
 	"github.com/tinfoilsh/tinfoil-go"
 )
 
@@ -23,12 +24,12 @@ const (
 func main() {
 	cfg, err := LoadConfig()
 	if err != nil {
-		log.Fatal(err)
+		fatal("invalid configuration", err)
 	}
 
 	client, err := tinfoil.NewClient(option.WithAPIKey(cfg.TinfoilAPIKey))
 	if err != nil {
-		log.Fatalf("Failed to create Tinfoil client: %v", err)
+		fatal("failed to create Tinfoil client", err)
 	}
 
 	service := NewService(cfg,
@@ -61,17 +62,22 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
-		log.Infof("Starting on %s (model: %s, workers: %d)", cfg.ListenAddr, cfg.SafeguardModel, cfg.Workers)
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatal(err)
+		slog.Info("starting", "addr", cfg.ListenAddr, "model", cfg.SafeguardModel, "review_model", cfg.SafeguardReviewModel, "workers", cfg.Workers)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			fatal("server failed", err)
 		}
 	}()
 
 	<-sigChan
-	log.Info("Shutting down...")
+	slog.Info("shutting down")
 	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
 	defer cancel()
 	server.Shutdown(ctx)
 	stopWorkers()
 	workers.Wait()
+}
+
+func fatal(msg string, err error) {
+	slog.Error(msg, "error", err)
+	os.Exit(1)
 }
